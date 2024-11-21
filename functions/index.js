7a8a4bf2347ea8630a3c7a242717dacde19c0bd2
Gabriel Abade <161393183+gabrielvitorabade@users.noreply.github.com>
2024-11-20 23:44:23 -0300

    Implementação e inicio de reestruturação da função para gerar token e  envio automatico de mail.

--- functions/index.js
+++ functions/index.js
@@ -31,33 +31,6 @@ export function login(req, res) {
     });
 }
 export function redefinirSenha(req, res) {
-    let transport = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
-
-    let mailOptions = {
-        from: '',
-        to: 'myfriend@yahoo.com',
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
-    };
-
-    transport.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent: ' + info.response);
-        }
-    });
-
-    return res.end(JSON.stringify({ message: 'Email enviado com sucesso!' }));
-
-
 
     let body = '';
     req.on('data', chunk => {
@@ -71,19 +44,6 @@ export function redefinirSenha(req, res) {
             return res.end(JSON.stringify({ message: 'Email não informado' }));
         }
 
-        if (!body.senha) {
-            res.writeHead(400);
-            return res.end(JSON.stringify({ message: 'Senha não informada' }));
-        }
-        let senha = body.senha.toString();
-
-        if (body.senha.length < 6) {
-            res.writeHead(400);
-            return res.end(JSON.stringify({ message: 'A deve ter no mínimo 6 caracteres' }));
-        }
-
-        senha = crypto.createHash('sha256').update(senha).digest('hex');
-
         connection.query(`SELECT * FROM users WHERE email = ?`, [body.email], function (err, results) {
 
             if (err) {
@@ -91,22 +51,77 @@ export function redefinirSenha(req, res) {
                 return res.end(JSON.stringify({ message: 'Usuário não encontrado!' }));
             }
             if (results.length > 0) {
-                if (results[0].password === senha) {
+
+                function generateToken() { return crypto.randomBytes(20).toString('hex'); }
+                const token = generateToken();
+                let transport = nodemailer.createTransport({
+                    host: "sandbox.smtp.mailtrap.io",
+                    port: 2525,
+                    auth: {
+                        user: process.env.EMAIL_USER,
+                        pass: process.env.EMAIL_PASS
+                    }
+                });
+
+                let mailOptions = {
+                    from: '',
+                    to: 'myfriend@yahoo.com',
+                    subject: 'Sending Email using Node.js',
+                    text: ` TOKEN para redefinição de senha! 
+                    
+                    Siga as orientações abaixo. ☻
+
+                        Para concluir a solicitação de redefinição da sua senha, copie e cole o seguinte token na barra 'token' em sua página de redefinição.
+                            TOKEN: ${token} 
+                            Esse Token tem validade de 1 hora a partir do envio deste mail.
+                            Caso expire o prazo determinado, uma nova solicitação deve ser feita. `
+                };
+
+                transport.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
+
+                res.end(JSON.stringify({ message: 'Email enviado com sucesso!' }));
+
+                if (token === body.token) {
+                    body.senha.removeatribute("hidden")
+                    if (!body.senha) {
+                        res.writeHead(400);
+                        return res.end(JSON.stringify({ message: 'Senha não informada' }));
+                    }
+                    let senha = body.senha.toString();
+
+                    if (body.senha.length < 6) {
+                        res.writeHead(400);
+                        return res.end(JSON.stringify({ message: 'A deve ter no mínimo 6 caracteres' }));
+                    }
+
+                    senha = crypto.createHash('sha256').update(senha).digest('hex');
+                    if (results[0].password === senha) {
+                        res.writeHead(400);
+                        return res.end(JSON.stringify({ message: 'A nova senha não pode ser igual a antiga!' }));
+                    }
+
+                    connection.query('UPDATE users SET password = ? WHERE email = ?', [senha, body.email], function (err, updateResults) {
+                        if (err) {
+                            res.writeHead(500);
+                            return res.end(JSON.stringify({ message: 'Erro ao atualizar senha' }));
+                        }
+                        res.writeHead(200);
+                        return res.end(JSON.stringify({ message: 'Senha redefinida com sucesso!' }));
+                    });
+                } else {
                     res.writeHead(400);
                     return res.end(JSON.stringify({ message: 'A nova senha não pode ser igual a antiga!' }));
                 }
 
-                connection.query('UPDATE users SET password = ? WHERE email = ?', [senha, body.email], function (err, updateResults) {
-                    if (err) {
-                        res.writeHead(500);
-                        return res.end(JSON.stringify({ message: 'Erro ao atualizar senha' }));
-                    }
-                    res.writeHead(200);
-                    return res.end(JSON.stringify({ message: 'Senha redefinida com sucesso!' }));
-                });
             } else {
                 res.writeHead(401);
-                return res.end(JSON.stringify({ message: 'Usuário inválido!' }));
+                return res.end(JSON.stringify({ message: 'Token inválido, tente novamente!' }));
             }
 
         })
