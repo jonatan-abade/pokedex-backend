7d181d3415ce954bed09da091a94702fd16d624d
Gabriel Abade <161393183+gabrielvitorabade@users.noreply.github.com>
2024-11-28 21:33:52 -0300

    atualização de mensagem de envio de email.

--- functions/index.js
+++ functions/index.js
@@ -30,66 +30,70 @@ export function login(req, res) {
         });
     });
 }
-export function sendmail(req,res){
+export function sendmail(req, res) {
     let body = '';
     req.on('data', chunk => {
         body += chunk.toString();
     });
     req.on('end', () => {
         body = JSON.parse(body);
-        
+
         if (!body.email) {
             res.writeHead(400);
             return res.end(JSON.stringify({ message: 'Email não informado' }));
         }
 
-        
+
         connection.query(`SELECT * FROM users WHERE email = ?`, [body.email], function (err, results) {
-            
-        
+
+
             if (results.length > 0) {
 
-        function generateToken() { return crypto.randomBytes(20).toString('hex'); }
+                function generateToken() { return crypto.randomBytes(20).toString('hex'); }
 
-        const token = generateToken();
+                const token = generateToken();
 
-        let transport = nodemailer.createTransport({
-            host: "sandbox.smtp.mailtrap.io",
-            port: 2525,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASS
-            }
-        });
+                let transport = nodemailer.createTransport({
+                    host: "sandbox.smtp.mailtrap.io",
+                    port: 2525,
+                    auth: {
+                        user: process.env.EMAIL_USER,
+                        pass: process.env.EMAIL_PASS
+                    }
+                });
 
-        let mailOptions = {
-            from: '',
-            to: 'myfriend@yahoo.com',
-            subject: 'Sending Email using Node.js',
-            text: ` TOKEN para redefinição de senha! 
+                let mailOptions = {
+                    from: '',
+                    to: 'myfriend@yahoo.com',
+                    subject: 'Sending Email using Node.js',
+                    text: `TOKEN para redefinição de senha! 
             
-            Siga as orientações abaixo. ☻
+                     Siga as orientações abaixo. ☻
 
-                Para concluir a redefinição da sua senha, copie e cole o seguinte token na barra 'token' em sua página de redefinição.
+                        Para concluir a redefinição da sua senha, copie e cole o seguinte token na barra 'token' em sua página de redefinição.
+
                     TOKEN: ${token} 
         
-                    Caso erre o preenchimento do token na aplição, uma nova solicitação deve ser feita. `
-        };
+                        Caso erre o preenchimento do token na aplição, uma nova solicitação deve ser feita. `
+                };
 
-        transport.sendMail(mailOptions, function (error, info) {
-            if (error) {
-                console.log(error);
+                transport.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        console.log(error);
+                    } else {
+                        console.log('Email sent: ' + info.response);
+                    }
+                });
+
+                res.end(JSON.stringify({ message: 'Email enviado com sucesso!' }));
+
             } else {
-                console.log('Email sent: ' + info.response);
+                res.writeHead(500);
+                return res.end(JSON.stringify({ message: 'Email inválido!' }));
+
             }
         });
 
-        res.end(JSON.stringify({ message: 'Email enviado com sucesso!' }));
-    
-    }else{res.writeHead(500);
-                return res.end(JSON.stringify({ message: 'Email inválido!'}));
-
-    }});
     })
 }
 export function redefinirSenha(req, res) {
@@ -108,41 +112,38 @@ export function redefinirSenha(req, res) {
 
         connection.query(`SELECT * FROM users WHERE email = ?`, [body.email], function (err, results) {
 
-            if (err) {
+            if (results.length > 0) {
+
+                if (!body.senha) {
+                    res.writeHead(400);
+                    return res.end(JSON.stringify({ message: 'Senha não informada' }));
+                }
+                let senha = body.senha.toString();
+
+                if (body.senha.length < 6) {
+                    res.writeHead(400);
+                    return res.end(JSON.stringify({ message: 'A senha deve ter no mínimo 6 caracteres' }));
+                }
+
+                senha = crypto.createHash('sha256').update(senha).digest('hex');
+                if (results[0].password === senha) {
+                    res.writeHead(400);
+                    return res.end(JSON.stringify({ message: 'A nova senha não pode ser igual a antiga!' }));
+                }
+
+                connection.query('UPDATE users SET password = ? WHERE email = ?', [senha, body.email], function (err, updateResults) {
+                    if (err) {
+                        res.writeHead(500);
+                        return res.end(JSON.stringify({ message: 'Erro ao atualizar senha' }));
+                    }
+                    res.writeHead(200);
+                    return res.end(JSON.stringify({ message: 'Senha redefinida com sucesso!' }));
+                });
+            } else {
                 res.writeHead(500);
                 return res.end(JSON.stringify({ message: 'Usuário não encontrado!' }));
             }
-            if (results.length > 0) {
-
-                    if (!body.senha) {
-                        res.writeHead(400);
-                        return res.end(JSON.stringify({ message: 'Senha não informada' }));
-                    }
-                    let senha = body.senha.toString();
-
-                    if (body.senha.length < 6) {
-                        res.writeHead(400);
-                        return res.end(JSON.stringify({ message: 'A deve ter no mínimo 6 caracteres' }));
-                    }
-
-                    senha = crypto.createHash('sha256').update(senha).digest('hex');
-                    if (results[0].password === senha) {
-                        res.writeHead(400);
-                        return res.end(JSON.stringify({ message: 'A nova senha não pode ser igual a antiga!' }));
-                    }
-
-                    connection.query('UPDATE users SET password = ? WHERE email = ?', [senha, body.email], function (err, updateResults) {
-                        if (err) {
-                            res.writeHead(500);
-                            return res.end(JSON.stringify({ message: 'Erro ao atualizar senha' }));
-                        }
-                        res.writeHead(200);
-                        return res.end(JSON.stringify({ message: 'Senha redefinida com sucesso!' }));
-                    });
-               
-
-
-    }});
+        });
 
     })
 }
